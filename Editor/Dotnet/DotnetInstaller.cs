a091942ad267f6be830565629aa41add052304f8
gilzoide <gilzoide@gmail.com>
2024-05-16 19:00:11 -0300

    Add dotnet installer script

---
+++ Editor/Dotnet/DotnetInstaller.cs
@@ -0,0 +1,110 @@
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+
+namespace Gilzoide.FSharp.Editor
+{
+    public class DotnetInstaller
+    {
+        public const string DotnetInstallDirKey = "dotnet-sdk-install-dir";
+        public const string DotnetInstallDirDefault = "Library/dotnet-sdk-install-dir";
+        public const string DotnetSdkVersion = "8.0.300";
+
+        public static string DotnetInstallDir
+        {
+            get
+            {
+                string value = EditorPrefs.GetString(DotnetInstallDirKey, DotnetInstallDirDefault);
+                if (string.IsNullOrEmpty(value))
+                {
+                    value = DotnetInstallDirDefault;
+                }
+                return value;
+            }
+            set => EditorPrefs.SetString(DotnetInstallDirKey, value);
+        }
+
+#if UNITY_EDITOR_WIN
+        public static string DotnetInstallScriptName = "dotnet-install.ps1";
+#else
+        public static string DotnetInstallScriptName = "dotnet-install.sh";
+#endif
+        public static string DotnetInstallScriptPath => AssetDatabase.FindAssets($"glob:{DotnetInstallScriptName}", null)
+            .Select(AssetDatabase.GUIDToAssetPath)
+            .First();
+
+        [MenuItem("Tools/Dotnet/Install latest SDK")]
+        public static Task<bool> InstallDotnetSdk()
+        {
+            return InstallDotnetSdk(DotnetSdkVersion);
+        }
+
+        public static async Task<bool> InstallDotnetSdk(string sdkVersion)
+        {
+            if (Directory.Exists($"{DotnetInstallDir}/sdk")
+                && (string.IsNullOrEmpty(sdkVersion) || Directory.Exists($"{DotnetInstallDir}/sdk/{sdkVersion}")))
+            {
+                Debug.Log($"Already installed in '{DotnetInstallDir}/sdk'");
+                return true;
+            }
+
+            var arguments = new List<string>
+            {
+                "--install-dir",
+                $"'{DotnetInstallDir}'",
+            };
+            if (!string.IsNullOrEmpty(sdkVersion))
+            {
+                arguments.Add("--version");
+                arguments.Add($"'{sdkVersion}'");
+            }
+
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = DotnetInstallScriptPath,
+                Arguments = string.Join(" ", arguments),
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+            };
+            using (var process = Process.Start(processInfo))
+            {
+                process.OutputDataReceived += (sender, args) =>
+                {
+                    if (!string.IsNullOrEmpty(args.Data))
+                    {
+                        Debug.Log(args.Data);
+                    }
+                };
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    if (!string.IsNullOrEmpty(args.Data))
+                    {
+                        Debug.LogError(args.Data);
+                    }
+                };
+
+                var progressId = Progress.Start("Installing dotnet SDK");
+                try
+                {
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+                    while (!process.HasExited)
+                    {
+                        Progress.Report(progressId, 0);
+                        await Task.Yield();
+                    }
+                }
+                finally
+                {
+                    Progress.Remove(progressId);
+                }
+                return process.ExitCode == 0;
+            }
+        }
+    }
+}
